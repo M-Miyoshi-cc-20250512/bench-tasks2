8ed6c5dfcb8c902c286fdcbd54216a04eaea2b7b
M-Miyoshi-cc-20250512 <m-miyoshi@care-con.co.jp>
2025-12-24 18:24:38 +0900

    12/24作業分

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import NewsViewSet
+from .views import NewsViewSet, hello_api, news_title_list, latest_news
 
 router = DefaultRouter()
 router.register(r'', NewsViewSet)
 
 urlpatterns = [
+    path('news/hello/', hello_api),
+    path('news/title/', news_title_list),
+    path('news/latest/<int:count>/', latest_news),
     path('news/', include(router.urls)),
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,7 +1,26 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import News
 from .serializers import NewsSerializer
 from rest_framework import viewsets
 
+
 class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = News.objects.order_by('-published_at').all()
     serializer_class = NewsSerializer
+
+
+@api_view(['GET'])
+def hello_api(request):
+    return Response({"message": "hello api"})
+
+@api_view(['GET'])
+def news_title_list(request):
+    titles = News.objects.values_list('title', flat=True)
+    return Response(titles)
+
+@api_view(['GET'])
+def latest_news(request, count):
+    news = News.objects.order_by('-published_at')[:count]
+    serializer = NewsSerializer(news, many=True)
+    return Response(serializer.data)
